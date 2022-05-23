74b2f87e16826580bd4ccc92ae12d437d3cd1c84
Danil Pobirskiy <danilpobiryan@yandex.ru>
2022-05-23 23:33:17 +0800

    first step

---
+++ src/gendiff.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+import { Command } from 'commander';
+
+const program = new Command();
+
+program
+  .version('0.1')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format <type>', 'output format', 'stylish')
+
+
+program.parse();
