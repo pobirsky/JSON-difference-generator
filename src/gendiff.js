df8fd6742604a46261061d43449de1f5a9c43ba1
Danil Pobirskiy <danilpobiryan@yandex.ru>
2022-05-25 12:08:42 +0800

    step 2 add gendiff.js

--- src/gendiff.js
+++ src/gendiff.js
@@ -4,9 +4,9 @@ import { Command } from 'commander';
 const program = new Command();
 
 program
-  .version('0.1')
+  .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format <type>', 'output format', 'stylish')
+  .argument('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format')
+  .parse(process.argv);
 
-
-program.parse();
