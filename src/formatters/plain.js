43f32e47a474728222e9bbe38841a794fe4c55e8
Danil Pobirskiy <danilpobiryan@yandex.ru>
2022-06-17 13:31:33 +0800

    add plain refact

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,39 +1,40 @@
-const formatValue = (val) => {
+const format = (val) => {
+  if (typeof val === 'object') {
+    return '[complex value]';
+  }
   if (typeof val === 'string') {
     return `'${val}'`;
   }
   if (val === null) {
     return null;
   }
-  if (typeof val === 'object') {
-    return '[complex value]';
-  }
   return val;
 };
 
-const plainFormatter = (data) => {
+const plain = (data) => {
   const iter = (children, parent) => {
     const diffColl = children.flatMap((node) => {
       const newPath = parent ? `${parent}.${node.name}` : `${node.name}`;
       switch (node.type) {
         case 'nested':
           return iter(node.children, newPath);
+        // если узел не менялся не выводим []
         case 'unchanged':
           return [];
         case 'deleted':
           return `Property '${newPath}' was removed`;
         case 'added':
-          return `Property '${newPath}' was added with value: ${formatValue(node.value1)}`;
+          return `Property '${newPath}' was added with value: ${format(node.value2)}`;
         case 'changed':
-          return `Property '${newPath}' was updated. From ${formatValue(node.value1)} to ${formatValue(node.value2)}`;
+          return `Property '${newPath}' was updated. From ${format(node.value1)} to ${format(node.value2)}`;
         default:
-          return 'kek';
+          return null;
       }
-    })
+    });
     return diffColl.join('\n');
   };
   const result = iter(data, '');
   return result;
 };
 
-export default plainFormatter;
+export default plain;
