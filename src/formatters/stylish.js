04e8d8cf03825e721dedf89e768256bf5ca1d9bf
Danil Pobirskiy <danilpobiryan@yandex.ru>
2022-07-05 11:38:21 +0300

    refactore

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,55 +1,29 @@
-const getIndent = (currentDepth, multiplier = 4) => ' '.repeat(currentDepth * multiplier - 2);
+const getIndent = (currentDepth, multiplier = 4) => " ".repeat(currentDepth * multiplier - 2);
 
 const stringify = (value, depth) => {
-  if (typeof value !== 'object' || value === null) {
+  if (typeof value !== "object" || value === null) {
     return value;
   }
-
   const indent = getIndent(depth);
   const indentClose = getIndent(depth - 1);
-
   const entries = Object.entries(value);
-
-  const result = entries.map(
-    ([key, innerValue]) => `${indent}  ${key}: ${stringify(innerValue, depth + 1)}`,
-  );
-  return ['{', ...result, `${indentClose}  }`].join('\n');
+  const result = entries.map(([key, innerValue]) => `${indent}  ${key}: ${stringify(innerValue, depth + 1)}`);
+  return ["{", ...result, `${indentClose}  }`].join("\n");
 };
 
-const sign = {
-  added: '+',
-  deleted: '-',
-  unchanged: ' ',
+const mapper = {
+  nested: ({ name, children }, depth, fn) => `${getIndent(depth)}  ${name}: {\n${fn(children, depth + 1)}\n${getIndent(depth)}  }`,
+  added: ({ name, value }, depth) => `${getIndent(depth)}+ ${name}: ${stringify(value, depth + 1)}`,
+  deleted: ({ name, value }, depth) => `${getIndent(depth)}- ${name}: ${stringify(value, depth + 1)}`,
+  changed: ({ name, value1, value2 }, depth) => {
+    const oldVal = `${getIndent(depth)}- ${name}: `;
+    const newVal = `${getIndent(depth)}+ ${name}: `;
+    return `${oldVal}${stringify(value1, depth + 1)}\n${newVal}${stringify(value2, depth + 1)}`;
+  },
+  unchanged: ({ name, value }, depth) => `${getIndent(depth)}  ${name}: ${stringify(value, depth + 1)}`,
 };
 
-const formatter = (tree) => {
-  const iter = (node, depth) => {
-    const arrMap = node.map((key) => {
-      const indent = getIndent(depth);
-
-      const makeLine = (value, mark) => `${indent}${mark} ${key.name}: ${stringify(value, depth + 1)}`;
-
-      switch (key.type) {
-        case 'unchanged': {
-          return makeLine(key.value, sign.unchanged);
-        }
-        case 'deleted': {
-          return makeLine(key.value, sign.deleted);
-        }
-        case 'added': {
-          return makeLine(key.value, sign.added);
-        }
-        case 'changed': {
-          return `${makeLine(key.value1, sign.deleted)}\n${makeLine(key.value2, sign.added)}`;
-        }
-        default: {
-          return `${indent}  ${key.name}: {\n${iter(key.children, depth + 1)}\n${indent}  }`;
-        }
-      }
-    });
-    return arrMap.join('\n');
-  };
+export default (tree) => {
+  const iter = (ast, depth) => ast.map((node) => mapper[node.type](node, depth, iter)).join('\n');
   return `{\n${iter(tree, 1)}\n}`;
 };
-
-export default formatter;
