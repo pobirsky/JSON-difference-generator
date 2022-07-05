c26379d6dc44c1fe814692dbc8e6c04b0079bccd
Danil Pobirskiy <danilpobiryan@yandex.ru>
2022-07-05 22:49:43 +0300

    Refactore plain

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -16,12 +16,13 @@ const newPath = (parent, current) => (parent === '' ? current : parent.concat(`.
 const mapper = {
   nested: ({ name, children }, depth, fn) => fn(children, newPath(depth, name)),
   added: ({ name, value }, depth) => `Property '${newPath(depth, name)}' was added with value: ${stringify(value)}`,
-  removed: ({ name }, depth) => `Property '${newPath(depth, name)}' was removed`,
+  deleted: ({ name }, depth) => `Property '${newPath(depth, name)}' was removed`,
   changed: ({ name, value1, value2 }, depth) => `Property '${newPath(depth, name)}' was updated. From ${stringify(value1)} to ${stringify(value2)}`,
-  unchanged: () => null,
 };
 
 export default (tree) => {
-  const iter = (ast, depth) => ast.map((node) => mapper[node.type](node, depth, iter)).join('\n');
+  const iter = (ast, depth) => ast
+    .filter((node) => node.type !== 'unchanged')
+    .map((node) => mapper[node.type](node, depth, iter)).join('\n');
   return iter(tree, '');
 };
